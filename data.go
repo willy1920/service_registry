07d84da809193dd910d8a8f320d9af1073555951
Willy <willychai05@gmail.com>
2020-11-09 10:43:05 +0000

    init new table

--- data.go
+++ data.go
@@ -14,4 +14,18 @@ func (self *Database) InitData() {
 	var err error
 	self.Con, err = sql.Open("sqlite3", "data.sqlite3")
 	checkErr(err)
+
+	sqlstmt := `CREATE TABLE service_registry(
+		name TEXT NOT NULL,
+		hostname TEXT NOT NULL,
+		ipAddr TEXT NOT NULL,
+		status TEXT NOT NULL,
+		port INT NOT NULL,
+		healthCheckUrl TEXT NOT NULL,
+		PRIMARY KEY('name', 'ipAddr')
+	)`
+	stmt, err := self.Con.Prepare(sqlstmt)
+	checkErr(err)
+
+	stmt.Exec()
 }
